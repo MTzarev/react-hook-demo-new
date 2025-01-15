da0a643f6567140220c238f5dca1b66628c67923
Miroslav Tzarev <mtzarev@gmail.com>
2025-01-15 14:48:39 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import './App.css';
 import {useEffect, useState} from 'react'
 function App() {
   let [name, setName] = useState('');
+  let [count, setCount] = useState(0)
 
   useEffect(()=>{
     setTimeout(()=>{
-      setName('Gosho')
+      setName('Fast Click Game')
     }, 1500);
   },[])
+  let clicker = ()=>setCount(x=>x+1)
   return(
     <div className="App">
-    <h1>{name}</h1>
+    <h1>{!name? 'Loading... ': name}</h1>
+    <h2>{count}</h2>
+    <button onClick={clicker}>+</button>
     </div>
   );
 }
